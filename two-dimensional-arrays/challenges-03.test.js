dec91b38b229cb970ec41f6c8eb6823c086659a1
Evan Slaton <evanslaton@gmail.com>
2018-09-12 17:38:29 -0700

    Finished challenge 2

--- two-dimensional-arrays/challenges-03.test.js
+++ two-dimensional-arrays/challenges-03.test.js
@@ -35,7 +35,7 @@ const grandTotal = (stores) => {
   }
 
   return totalCookiesPerHour;
-}
+};
 
 
 
@@ -52,6 +52,18 @@ const grandTotal = (stores) => {
 
 const salesData = (hours, data) => {
   // Solution code here...
+  const hourlySalesData = [];
+
+  data.forEach((data, index) => {
+    const salesInfo = {
+      sales: `${data} cookies`,
+      time: hours[index],
+    };
+
+    hourlySalesData.push(salesInfo);
+  });
+
+  return hourlySalesData;
 };
 
 // ------------------------------------------------------------------------------------------------
@@ -233,26 +245,26 @@ describe('Testing challenge 1', () => {
   });
 });
 
-// describe('Testing challenge 2', () => {
-//   test('It should create an object of data for each store', () => {
-//     expect(salesData(hoursOpen, grandTotal(cookieStores))).toStrictEqual([
-//       { sales: '88 cookies', time: '9 a.m.' },
-//       { sales: '153 cookies', time: '10 a.m.' },
-//       { sales: '252 cookies', time: '11 a.m.' },
-//       { sales: '286 cookies', time: '12 p.m.' },
-//       { sales: '139 cookies', time: '1 p.m.' },
-//       { sales: '161 cookies', time: '2 p.m.' },
-//       { sales: '145 cookies', time: '3 p.m.' },
-//       { sales: '232 cookies', time: '4 p.m.' },
-//       { sales: '276 cookies', time: '5 p.m.' },
-//       { sales: '207 cookies', time: '6 p.m.' },
-//       { sales: '161 cookies', time: '7 p.m.' },
-//       { sales: '169 cookies', time: '8 p.m.' }
-//     ]);
+describe('Testing challenge 2', () => {
+  test('It should create an object of data for each store', () => {
+    expect(salesData(hoursOpen, grandTotal(cookieStores))).toStrictEqual([
+      { sales: '88 cookies', time: '9 a.m.' },
+      { sales: '153 cookies', time: '10 a.m.' },
+      { sales: '252 cookies', time: '11 a.m.' },
+      { sales: '286 cookies', time: '12 p.m.' },
+      { sales: '139 cookies', time: '1 p.m.' },
+      { sales: '161 cookies', time: '2 p.m.' },
+      { sales: '145 cookies', time: '3 p.m.' },
+      { sales: '232 cookies', time: '4 p.m.' },
+      { sales: '276 cookies', time: '5 p.m.' },
+      { sales: '207 cookies', time: '6 p.m.' },
+      { sales: '161 cookies', time: '7 p.m.' },
+      { sales: '169 cookies', time: '8 p.m.' }
+    ]);
 
-//     expect(salesData(hoursOpen, grandTotal(cookieStores)).length).toStrictEqual(hoursOpen.length);
-//   });
-// });
+    expect(salesData(hoursOpen, grandTotal(cookieStores)).length).toStrictEqual(hoursOpen.length);
+  });
+});
 
 // describe('Testing challenge 3', () => {
 //   test('It should return a list of valentine exchanges', () => {
