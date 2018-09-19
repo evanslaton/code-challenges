212c6017f464ef76ff0e7be98bacc9cc879ea097
Evan Slaton <evanslaton@gmail.com>
2018-09-18 18:24:47 -0700

    Finished challenge 6

--- map/challenges-07.test.js
+++ map/challenges-07.test.js
@@ -118,6 +118,7 @@ const snorlaxAbilities = {
 
 const extractAbilities = (abilities) => {
   // Solution code here...
+  return abilities.map((obj) => obj.ability.name);
 };
 
 /*------------------------------------------------------------------------------------------------
@@ -218,34 +219,34 @@ Run your tests from the console: jest challenges-07.test.js
 //   });
 // });
 
-describe('Testing challenge 5', () => {
-  test('It should return an array containing the keys from an object', () => {
-    expect(evenOdd([5, 8, 2, 6, 9, 13, 542, 541])).toStrictEqual([ 'odd', 'even', 'even', 'even', 'odd', 'odd', 'even', 'odd' ]);
-    expect(evenOdd([5, 8, 2, 6, 9, 13, 542, 541]).length).toStrictEqual(8);
-  });
+// describe('Testing challenge 5', () => {
+//   test('It should return an array containing the keys from an object', () => {
+//     expect(evenOdd([5, 8, 2, 6, 9, 13, 542, 541])).toStrictEqual([ 'odd', 'even', 'even', 'even', 'odd', 'odd', 'even', 'odd' ]);
+//     expect(evenOdd([5, 8, 2, 6, 9, 13, 542, 541]).length).toStrictEqual(8);
+//   });
 
-  test('It should work with all odd numbers', () => {
-    expect(evenOdd([1, 3, 5, 7, 9])).toStrictEqual([ 'odd', 'odd', 'odd', 'odd', 'odd' ]);
-    expect(evenOdd([1, 3, 5, 7, 9]).length).toStrictEqual(5);
-  });
+//   test('It should work with all odd numbers', () => {
+//     expect(evenOdd([1, 3, 5, 7, 9])).toStrictEqual([ 'odd', 'odd', 'odd', 'odd', 'odd' ]);
+//     expect(evenOdd([1, 3, 5, 7, 9]).length).toStrictEqual(5);
+//   });
 
-  test('It should work with all even numbers', () => {
-    expect(evenOdd([2, 4, 6, 8, 10])).toStrictEqual([ 'even', 'even', 'even', 'even', 'even' ]);
-    expect(evenOdd([2, 4, 6, 8, 10]).length).toStrictEqual(5);
-  });
+//   test('It should work with all even numbers', () => {
+//     expect(evenOdd([2, 4, 6, 8, 10])).toStrictEqual([ 'even', 'even', 'even', 'even', 'even' ]);
+//     expect(evenOdd([2, 4, 6, 8, 10]).length).toStrictEqual(5);
+//   });
 
-  test('It should return the string "N/A" if a non-number is included in the array', () => {
-    expect(evenOdd([5, 8, 2, 'hi'])).toStrictEqual([ 'odd', 'even', 'even', 'N/A' ]);
-    expect(evenOdd([5, 8, 2, 'hi']).length).toStrictEqual(4);
-  });
-});
-
-// describe('Testing challenge 6', () => {
-//   test('It should return an array containing only the ability names', () => {
-//     expect(extractAbilities(snorlaxAbilities.abilities)).toStrictEqual(['gluttony', 'cute charm', 'immunity']);
-//     expect(extractAbilities(snorlaxAbilities.abilities).length).toStrictEqual(3);
+//   test('It should return the string "N/A" if a non-number is included in the array', () => {
+//     expect(evenOdd([5, 8, 2, 'hi'])).toStrictEqual([ 'odd', 'even', 'even', 'N/A' ]);
+//     expect(evenOdd([5, 8, 2, 'hi']).length).toStrictEqual(4);
 //   });
 // });
+
+describe('Testing challenge 6', () => {
+  test('It should return an array containing only the ability names', () => {
+    expect(extractAbilities(snorlaxAbilities.abilities)).toStrictEqual(['gluttony', 'cute charm', 'immunity']);
+    expect(extractAbilities(snorlaxAbilities.abilities).length).toStrictEqual(3);
+  });
+});
 
 // describe('Testing challenge 7', () => {
 //   test('It should return an array containing objects with name and total values', () => {
